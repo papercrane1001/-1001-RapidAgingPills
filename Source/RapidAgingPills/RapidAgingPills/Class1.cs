a8c1c675c011f43bbc5d5ce24836d9567901d84d
papercrane1001 <papercrane1001@gmail.com>
2021-08-09 13:51:39 -0700

    I think it's basically all there.  Need to pull it together, of course.

--- Source/RapidAgingPills/RapidAgingPills/Class1.cs
+++ Source/RapidAgingPills/RapidAgingPills/Class1.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using RimWorld;
+using Verse;
+using UnityEngine;
+
 namespace RapidAgingPills
 {
-    public class Class1
+    //[DefOf]
+    //class AgingDefOf
+    //{
+    //    //public static HediffDef 
+
+    //}
+    public class IngestionOutcomeDoer_RapidAging : IngestionOutcomeDoer
     {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            //throw new NotImplementedException();
+            if(pawn == null) { return; }
+            pawn.ageTracker.DebugForceBirthdayBiological();
+            //pawn.ageTracker.DebugMakeOlder
+            //pawn.ageTracker.
+        }
     }
 }
