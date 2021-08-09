b6a41c00465bef86a84957d302597a4968dfc0db
papercrane1001 <papercrane1001@gmail.com>
2021-08-09 13:39:56 -0700

    Bunch of stuff

---
+++ Source/RapidAgingPills/RapidAgingPills/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidAgingPills
+{
+    public class Class1
+    {
+    }
+}
