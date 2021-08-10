39e2663ba0276c35ae933a7be9e132c11ddb7e64
papercrane1001 <papercrane1001@gmail.com>
2021-08-10 07:22:47 -0700

    Probably most of the way there.

---
+++ Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace RapidAgingPills
+{
+    //[DefOf]
+    //class AgingDefOf
+    //{
+    //    //public static HediffDef 
+
+    //}
+    public class IngestionOutcomeDoer_RapidAging : IngestionOutcomeDoer
+    {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            //throw new NotImplementedException();
+            if(pawn == null) { return; }
+            pawn.ageTracker.DebugForceBirthdayBiological();
+            //pawn.ageTracker.DebugMakeOlder
+            //pawn.ageTracker.
+        }
+    }
+}
