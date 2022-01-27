f2b9c97b2e7324e897f496862c56e39277e44f43
papercrane1001 <papercrane1001@gmail.com>
2022-01-27 14:38:48 -0800

    Learning, time for a break

--- Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
+++ Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
@@ -4,18 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using HarmonyLib;
 using RimWorld;
 using Verse;
 using UnityEngine;
 
 namespace RapidAgingPills
 {
-    //[DefOf]
-    //class AgingDefOf
-    //{
-    //    //public static HediffDef 
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        // this static constructor runs to create a HarmonyInstance and install a patch.
+        static HarmonyPatches()
+        {
+            //var harmony = Harmony.
+            var harmony = new Harmony("rimworld.roxxploxx.unificamagica");
 
-    //}
+            //HarmonyInstance harmony = HarmonyInstance.Create("rimworld.roxxploxx.unificamagica");
+
+            // find the FillTab method of the class RimWorld.ITab_Pawn_Character
+            System.Reflection.MethodInfo targetMethod = AccessTools.Method(typeof(RimWorld.ITab_Pawn_Character), "FillTab");
+            //MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.ITab_Pawn_Character), "FillTab");
+
+            // find the static method to call before (i.e. Prefix) the targetmethod
+            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(UnificaMagica.HarmonyPatches).GetMethod("FillTab_Prefix"));
+
+            // patch the targetmethod, by calling prefixmethod before it runs, with no postfixmethod (i.e. null)
+            harmony.Patch(targetmethod, prefixmethod, null);
+        }
+
+        // This method is now always called right before RimWorld.ITab_Pawn_Character.FillTab.
+        // So, before the ITab_Pawn_Character is instantiated, reset the height of the dialog window.
+        // The class RimWorld.ITab_Pawn_Character is static so there is no this __instance.
+        public static void FillTab_Prefix()
+        {
+            RimWorld.CharacterCardUtility.PawnCardSize.y = DefDatabase<RimWorld.SkillDef>.AllDefsListForReading.Count * 47.5f;
+        }
+    }
     public class IngestionOutcomeDoer_RapidAging : IngestionOutcomeDoer
     {
         protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
