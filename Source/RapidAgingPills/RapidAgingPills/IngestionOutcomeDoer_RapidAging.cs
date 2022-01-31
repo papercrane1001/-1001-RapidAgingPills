221df6de087fa2c474d168fd5b23238431ecc84f
papercrane1001 <papercrane1001@gmail.com>
2022-01-31 09:50:36 -0800

    Cleanup

--- Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
+++ Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
@@ -14,17 +14,12 @@ namespace RapidAgingPills
     [StaticConstructorOnStartup]
     static class HarmonyPatches
     {
-        // this static constructor runs to create a HarmonyInstance and install a patch.
         static HarmonyPatches()
         {
             
             var harmony = new Harmony("rimworld.OneThousandOne.RapidAgingPills");
 
             System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "DebugMakeOlder");
-
-            
-
-            //var mPostfix = SymbolExtensions.GetMethodInfo(() => DebugMakeOlder_Postfix());
             var mPostfix = AccessTools.Method(typeof(HarmonyPatches), "DebugMakeOlder_Postfix");
 
             harmony.Patch(mOriginal, null, new HarmonyMethod(mPostfix));
@@ -32,8 +27,6 @@ namespace RapidAgingPills
         public static void DebugMakeOlder_Postfix(ref Verse.Pawn_AgeTracker __instance)
         {
             System.Reflection.MethodInfo mCalculateGrowth = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "CalculateGrowth");
-            //var mDel = mCalculateGrowth.CreateDelegate();
-            //__instance.CalculateGrowth(60 * 60000);
             mCalculateGrowth.Invoke(__instance, new object[] { 60 * +60000 });
         }
 
@@ -45,9 +38,6 @@ namespace RapidAgingPills
         {
             if(pawn == null) { return; }
             pawn.ageTracker.DebugMakeOlder(60 * 60000);
-            //pawn.ageTracker.
-            //pawn.ageTracker.DebugForceBirthdayBiological();
-            //pawn.ageTracker.
         }
     }
 }
