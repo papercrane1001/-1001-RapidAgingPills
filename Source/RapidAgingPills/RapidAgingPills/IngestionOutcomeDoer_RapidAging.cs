f472dac032889fb0c0c38bea81cc59b68ebc60c7
papercrane1001 <papercrane1001@gmail.com>
2022-01-31 08:59:15 -0800

    Update so someone can help me

--- Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
+++ Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
@@ -17,39 +17,31 @@ namespace RapidAgingPills
         // this static constructor runs to create a HarmonyInstance and install a patch.
         static HarmonyPatches()
         {
-            //var harmony = Harmony.
-            var harmony = new Harmony("rimworld.roxxploxx.unificamagica");
+            ;
+            var harmony = new Harmony("");
 
-            //HarmonyInstance harmony = HarmonyInstance.Create("rimworld.roxxploxx.unificamagica");
+            System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "DebugMakeOlder");
 
-            // find the FillTab method of the class RimWorld.ITab_Pawn_Character
-            System.Reflection.MethodInfo targetMethod = AccessTools.Method(typeof(RimWorld.ITab_Pawn_Character), "FillTab");
-            //MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.ITab_Pawn_Character), "FillTab");
+            var mPostfix = SymbolExtensions.GetMethodInfo(() => DebugMakeOlder_Postfix());
 
-            // find the static method to call before (i.e. Prefix) the targetmethod
-            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(UnificaMagica.HarmonyPatches).GetMethod("FillTab_Prefix"));
-
-            // patch the targetmethod, by calling prefixmethod before it runs, with no postfixmethod (i.e. null)
-            harmony.Patch(targetmethod, prefixmethod, null);
+            harmony.Patch(mOriginal, null, new HarmonyMethod(mPostfix));
         }
-
-        // This method is now always called right before RimWorld.ITab_Pawn_Character.FillTab.
-        // So, before the ITab_Pawn_Character is instantiated, reset the height of the dialog window.
-        // The class RimWorld.ITab_Pawn_Character is static so there is no this __instance.
-        public static void FillTab_Prefix()
+        public static void DebugMakeOlder_Postfix()
         {
-            RimWorld.CharacterCardUtility.PawnCardSize.y = DefDatabase<RimWorld.SkillDef>.AllDefsListForReading.Count * 47.5f;
+            this.RecalculateLifeStageIndex();
         }
+
+
     }
     public class IngestionOutcomeDoer_RapidAging : IngestionOutcomeDoer
     {
         protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
         {
             if(pawn == null) { return; }
-            //pawn.ageTracker.DebugForceBirthdayBiological();
             pawn.ageTracker.DebugMakeOlder(60 * 60000);
             //pawn.ageTracker.
             //pawn.ageTracker.DebugForceBirthdayBiological();
+            //pawn.ageTracker.
         }
     }
 }
