8dc653c55f2b07faae4b66bffe8012ad3b941477
papercrane1001 <papercrane1001@gmail.com>
2022-10-25 14:04:53 -0700

    1.4 update fixed

--- Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
+++ Source/RapidAgingPills/RapidAgingPills/IngestionOutcomeDoer_RapidAging.cs
@@ -19,12 +19,12 @@ namespace RapidAgingPills
             
             var harmony = new Harmony("rimworld.OneThousandOne.RapidAgingPills");
 
-            System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "DebugMakeOlder");
-            var mPostfix = AccessTools.Method(typeof(HarmonyPatches), "DebugMakeOlder_Postfix");
+            System.Reflection.MethodInfo mOriginal = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "DebugForceBirthdayBiological");
+            var mPostfix = AccessTools.Method(typeof(HarmonyPatches), "DebugForceBirthdayBiological_Postfix");
 
             harmony.Patch(mOriginal, null, new HarmonyMethod(mPostfix));
         }
-        public static void DebugMakeOlder_Postfix(ref Verse.Pawn_AgeTracker __instance)
+        public static void DebugForceBirthdayBiological_Postfix(ref Verse.Pawn_AgeTracker __instance)
         {
             System.Reflection.MethodInfo mCalculateGrowth = AccessTools.Method(typeof(Verse.Pawn_AgeTracker), "CalculateGrowth");
             mCalculateGrowth.Invoke(__instance, new object[] { 60 * +60000 });
@@ -37,7 +37,9 @@ namespace RapidAgingPills
         protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
         {
             if(pawn == null) { return; }
-            pawn.ageTracker.DebugMakeOlder(60 * 60000);
+            //pawn.ageTracker.DebugMakeOlder(60 * 60000);
+            //pawn.ageTracker.AgeBiologicalTicks
+            pawn.ageTracker.DebugForceBirthdayBiological();
         }
     }
 }
